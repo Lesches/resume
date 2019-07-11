adee5ec99c784e1c01f3bda8a6cd861c2a218e18
sholom <sholomls@gmail.com>
2019-07-11 18:26:38 -0400

    create contact method

--- src/app/resume.service.ts
+++ src/app/resume.service.ts
@@ -1,9 +1,20 @@
 import { Injectable } from '@angular/core';
+import {HttpClient} from '@angular/common/http';
 
 @Injectable({
   providedIn: 'root'
 })
 export class ResumeService {
 
-  constructor() { }
+  constructor(private http: HttpClient) { }
+
+  createContact(name: string,
+                lname: string,
+                address: string,
+                email: string,
+                phone: string,
+                website: string,
+                social: string) {
+
+  }
 }
